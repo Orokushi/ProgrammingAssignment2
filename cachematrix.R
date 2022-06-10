5733138bdc808a423ca218d4145b41205f4a72bc
Zdenek Simunek <z.simunek@email.cz>
2022-06-10 14:18:54 +0200

    This is how its done.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#' This function implements matrix with catche.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse  <- function(inv) inverse <<- inv
+  getinverse  <- function() inverse
+  list(set = set, get = get,
+       setinverse  = setinverse ,
+       getinverse  = getinverse )
 }
 
 
 ## Write a short comment describing this function
-
+#' This function solves matrix. If matrix has been solved before, it uses catche.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
